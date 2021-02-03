0ddd379df9ba08696b6ae11d77a4e0f5db59466a
chunza2542 <hello@thechun.dev>
2021-02-03 19:10:19 +0700

    Implement AST printer test

--- TS/src/compiler/Expr.ts
+++ TS/src/compiler/Expr.ts
@@ -11,7 +11,41 @@ abstract class Expr {
   abstract accept<R>(visitor: Visitor<R>): R;
 }
 
-class Binary extends Expr {
+export class ASTPrinter implements Visitor<string> {
+  print(expr: Expr): string {
+    return expr.accept(this);
+  }
+
+  visitBinaryExpr(expr: Binary): string {
+    return this.parenthesize(expr.operator.lexeme, expr.left, expr.right);
+  }
+  visitGroupingExpr(expr: Grouping): string {
+    return this.parenthesize("group", expr.expression);
+  }
+  visitLiteralExpr(expr: Literal): string {
+    if (expr.value == null) return "nil";
+    return expr.value.toString();
+  }
+  visitUnaryExpr(expr: Unary): string {
+    return this.parenthesize(expr.operator.lexeme, expr.right);
+  }
+
+  parenthesize(name: string, ...exprs: Expr[]) {
+    let result = "";
+    result += "(";
+    result += name;
+
+    exprs.forEach((expr) => {
+      result += " ";
+      result += expr.accept(this);
+    });
+    result += ")";
+
+    return result;
+  }
+}
+
+export class Binary extends Expr {
   readonly left: Expr;
   readonly operator: Token;
   readonly right: Expr;
@@ -29,7 +63,7 @@ class Binary extends Expr {
   }
 }
 
-class Grouping extends Expr {
+export class Grouping extends Expr {
   readonly expression: Expr;
 
   constructor(expression: Expr) {
@@ -43,7 +77,7 @@ class Grouping extends Expr {
   }
 }
 
-class Literal extends Expr {
+export class Literal extends Expr {
   readonly value: any;
 
   constructor(value: any) {
@@ -57,7 +91,7 @@ class Literal extends Expr {
   }
 }
 
-class Unary extends Expr {
+export class Unary extends Expr {
   readonly operator: Token;
   readonly right: Expr;
 
--- TS/src/index.ts
+++ TS/src/index.ts
@@ -1,8 +1,11 @@
-import { Scanner } from "./compiler/Scanner";
+import { ASTPrinter, Binary, Unary, Literal, Grouping } from "./compiler/Expr";
+import { Token } from "./compiler/Token";
+import { TokenType } from "./const/TokenType";
 
-const sourceCode = `!*+-/=<> <= ==
-for 
-var name = "Chun Rapeepat";`;
-
-const scanner = new Scanner(sourceCode);
-console.log(scanner.scanTokens());
+const expression = new Binary(
+  new Unary(new Token(TokenType.MINUS, "-", null, 1), new Literal(123)),
+  new Token(TokenType.STAR, "*", null, 1),
+  new Grouping(new Literal(45.67))
+);
+const printer = new ASTPrinter();
+console.log("result:", printer.print(expression));
